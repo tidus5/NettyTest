463a9608fdc11ecd22c8721b8514f94dc5504f62
Tidus <tidus1215@gmail.com>
2017-07-13 00:15:46 +0800

    commit

--- src/main/java/com/tidus5/NettyTest/App.java
+++ src/main/java/com/tidus5/NettyTest/App.java
@@ -1,13 +1,18 @@
 package com.tidus5.NettyTest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+
+	final static Logger logger = LoggerFactory.getLogger(App.class);
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		logger.info("OK");
+	}
 }
