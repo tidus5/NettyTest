9fc5b1c3531f2e0c87f18c8e9c24f72f89cb079b
Tidus <tidus1215@gmail.com>
2017-07-13 22:21:20 +0800

    commit base Netty

---
+++ src/main/java/com/tidus5/NettyTest/server/HelloServer.java
@@ -0,0 +1,43 @@
+package com.tidus5.NettyTest.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class HelloServer {
+
+	/**
+	 * 服务端监听的端口地址
+	 */
+	private static final int portNumber = 7878;
+
+	public static void main(String[] args) throws InterruptedException {
+		// 开启两个事件循环组，事件循环组会自动构建EventLoop，服务器一般开启两个，提高效率
+		EventLoopGroup bossGroup = new NioEventLoopGroup();
+		EventLoopGroup workerGroup = new NioEventLoopGroup();
+		try {
+			// Netty的引导类，用于简化开发
+			ServerBootstrap b = new ServerBootstrap();
+			// 把事件循环组加入引导程序
+			b.group(bossGroup, workerGroup);
+			// 开启socket
+			b.channel(NioServerSocketChannel.class);
+			// 加入业务控制器，这里是加入一个初始化类，其中包含了很多业务控制器
+			b.childHandler(new HelloServerInitializer());
+
+			// 服务器绑定端口监听
+			ChannelFuture f = b.bind(portNumber).sync();
+			// 监听服务器关闭监听
+			f.channel().closeFuture().sync();
+
+			// 可以简写为
+			/* b.bind(portNumber).sync().channel().closeFuture().sync(); */
+		} finally {
+			// Netty优雅退出
+			bossGroup.shutdownGracefully();
+			workerGroup.shutdownGracefully();
+		}
+	}
+}
---
+++ src/main/java/com/tidus5/NettyTest/server/HelloServerHandler.java
@@ -0,0 +1,63 @@
+package com.tidus5.NettyTest.server;
+import java.net.InetAddress;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
+import io.netty.util.concurrent.GlobalEventExecutor;
+
+//继承Netty提供的通道传入处理器类，只要复写方法就可以了，简化开发
+public class HelloServerHandler extends SimpleChannelInboundHandler<String> {
+	
+	//获取现有通道，一个通道channel就是一个socket链接在这里
+	public static ChannelGroup channels = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
+
+    //有新链接加入，对外发布消息
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {  // (2)
+        Channel incoming = ctx.channel();
+        for (Channel channel : channels) {
+            channel.writeAndFlush("[SERVER] - " + incoming.remoteAddress() + " 加入\n");
+        }
+        channels.add(ctx.channel());
+    } 
+    
+    //有链接断开，对外发布消息
+    @Override
+    public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {  // (3)
+        Channel incoming = ctx.channel();
+        for (Channel channel : channels) {
+            channel.writeAndFlush("[SERVER] - " + incoming.remoteAddress() + " 离开\n");
+        }
+        channels.remove(ctx.channel());
+    }
+    
+    //消息读取有两个方法，channelRead和channelRead0，其中channelRead0可以读取泛型，常用  
+    //收到消息打印出来，并返还客户端消息
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
+        // 收到消息直接打印输出
+        System.out.println(ctx.channel().remoteAddress() + " Say : " + msg);
+        
+        // 返回客户端消息 - 我已经接收到了你的消息
+        ctx.writeAndFlush("Received your message !\n");
+    }
+    
+    /*
+     * 
+     * 覆盖 channelActive 方法 在channel被启用的时候触发 (在建立连接的时候)
+     * 
+     * channelActive 和 channelInActive 在后面的内容中讲述，这里先不做详细的描述
+     * */
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        
+        System.out.println("RamoteAddress : " + ctx.channel().remoteAddress() + " active !");
+        
+        ctx.writeAndFlush( "Welcome to " + InetAddress.getLocalHost().getHostName() + " service!\n");
+        
+        super.channelActive(ctx);
+    }
+}
